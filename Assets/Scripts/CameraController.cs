e7398df7d24519eb502043384f07cdb99e962604
Quentin Strinati <qstrinati@gmail.com>
2018-09-03 17:12:09 -0400

    Build V1
    -UFO moves with arrows
    -Score by collecting gold nuggets

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CameraController : MonoBehaviour
+{
+
+	public GameObject player;
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate ()	// Runs after all items have been processed in Update.
+						// Ensures the player has already moved for that frame.
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;
+	public Text scoreText;
+	public Text winText;
+
+	private Rigidbody2D rb2d;
+	private int score;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		score = 0;
+		winText.text = "";
+		SetScoreText();
+	}
+
+	void FixedUpdate() // For physics code
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		
+		rb2d.AddForce(movement*speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			score++;
+			SetScoreText();
+		}
+	}
+
+	void SetScoreText()
+	{
+		scoreText.text = "Score: " + score.ToString();
+		if (score >= 12)
+		{
+			winText.text = "You win !";
+		}
+	}
+}
